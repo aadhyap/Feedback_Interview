6eda59ff334d68c058acf464117c005a35677393
aadhyap <putturaadhya@gmail.com>
2018-05-19 10:45:41 -0400

    first camera code
    this is the beginning code

---
+++ camera-test.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+face_cascade = cv2.CascadeClassifier('./OpenCV-Python-Series/src/cascades/data/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame, reading frame by frame
+    ret, frame = cap.read() 
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor =1.5, minNeighbors = 5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    
+    #closes frame
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
